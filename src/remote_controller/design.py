063974df1c64d5ed899794a27eea30b421ad07f3
Vincent Vandyck <vincent.vandyck@gmail.com>
2018-12-06 12:34:50 +0100

    regenerated file

--- src/remote_controller/design.py
+++ src/remote_controller/design.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'src/remote_controller/design.ui'
+# Form implementation generated from reading ui file 'design.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
