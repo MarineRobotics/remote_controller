02145a41ff7ba126aa6ff9c595182f35db3f32b1
Vincent Vandyck <vincent.vandyck@gmail.com>
2024-11-25 15:44:30 -0500

    added -h help argument and changed host and master-resolved arguments to fit

--- runcontainer.sh
+++ runcontainer.sh
@@ -4,42 +4,73 @@
 ROS_MASTER_URI=http://192.168.1.160:11311
 export UID_GID="$(id -u):$(id -g)"
 export UID="$(id -u)"
+
 # Parse command line arguments
-OPTS=`getopt -o i:m:h:b --long host-ip:,master:,hostname:,rebuild -n 'parse-options' -- "$@"`
+OPTS=$(getopt -o i:m:n:b:h --long host-ip:,master:,hostname:,rebuild,help -n 'parse-options' -- "$@")
 
 REBUILD=false
 
-if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+if [ $? != 0 ]; then
+    echo "Failed parsing options." >&2
+    exit 1
+fi
 
 eval set -- "$OPTS"
 
-# Create simple function to resolve hostname to ip
+# Create simple function to resolve hostname to IP
 resolve_hostname_to_ip() {
-    # Resolve hostname to ip
+    # Resolve hostname to IP
     hostname=$1
-    ip=$(getent hosts $hostname | awk '{ print $1 }')
-    echo $ip
+    ip=$(getent hosts "$hostname" | awk '{ print $1 }')
+    echo "$ip"
 }
 
 while true; do
-# The case statement inside the loop checks the current argument ($1). 
-# If the argument matches one of the option patterns (-ip, --host-ip, -m, --master, -h, --hostname), 
-# it sets the corresponding variable to the value of the next argument ($2), 
-# then it uses shift; shift; to remove these two arguments (option and its value) from the list and move on to the next pair.
     case "$1" in
-        -i | --host-ip ) ROS_IP="$2"; shift; shift ;;
-        -u | --master ) ROS_MASTER_URI="$2"; shift; shift ;;
-        -m | --master-host ) ROS_MASTER_URI="http://$(resolve_hostname_to_ip "$2"):11311"; shift; shift ;;
-        -h | --hostname ) ROS_HOSTNAME="$2"; shift; shift ;;
-        -b | --rebuild ) REBUILD=true; shift ;;
-        -- ) shift; break ;;
-        * ) break ;;
+        -i | --host-ip)
+            ROS_IP="$2"
+            shift 2
+            ;;
+        -m | --master)
+            ROS_MASTER_URI="$2"
+            shift 2
+            ;;
+        -r | --master-resolved)
+            ROS_MASTER_URI="http://$(resolve_hostname_to_ip "$2"):11311"
+            shift 2
+            ;;
+        -n | --hostname)
+            ROS_HOSTNAME="$2"
+            shift 2
+            ;;
+        -b | --rebuild)
+            REBUILD=true
+            shift
+            ;;
+        -h | --help)
+            echo "Usage: $0 [OPTIONS]"
+            echo ""
+            echo "Options:"
+            echo "  -i, --host-ip IP             Set the ROS_IP environment variable"
+            echo "  -m, --master URI             Set the ROS_MASTER_URI environment variable"
+            echo "  -r, --master-resolved HOST   Resolve hostname to IP and set ROS_MASTER_URI"
+            echo "  -n, --hostname HOSTNAME      Set the ROS_HOSTNAME environment variable"
+            echo "  -b, --rebuild                Rebuild the Docker container before running"
+            echo "  -h, --help                   Display this help message"
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
     esac
 done
 
 # Print rebuild flag in purple
 echo -e "\e[35mRebuild: $REBUILD\e[0m"
-
 
 if [ -z "$ROS_HOSTNAME" ] && [ -z "$ROS_IP" ]; then
     export ROS_HOSTNAME=$(hostname)
@@ -60,19 +91,20 @@ echo "ROS_IP: $ROS_IP"
 echo "ROS_HOSTNAME: $ROS_HOSTNAME"
 echo "ROS_MASTER_URI: $ROS_MASTER_URI"
 
-# Ask enter to continue, or any other key to exit
-read -p "Press enter to continue, or any other key to exit" -n 1 -r
+# Prompt user to continue or exit
+read -p "Press Enter to continue, or any other key to exit" -n 1 -r
 
-project=$(basename $(pwd))
-xhost +local:${project}_app_1
+project=$(basename "$(pwd)")
+xhost +local:"${project}_app_1"
 
 # Rebuild container if required
-if [ "$REBUILD" = true ] ; then
+if [ "$REBUILD" = true ]; then
     echo -e "\e[35mRebuilding container...\e[0m"
     docker-compose build
     echo -e "\e[35mCompleted!\e[0m"
 fi
+
 echo -e "\e[35mLaunching remote controller GUI...\e[0m"
 docker-compose up
 
-xhost -local:${project}_app_1
+xhost -local:"${project}_app_1"
