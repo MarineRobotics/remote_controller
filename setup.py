1d8f86a3fd83904bdd144946759d0b58ce9bf35c
Vincent Vandyck <vincent.vandyck@gmail.com>
2025-04-29 13:23:29 -0400

    turned package into ROS2 package

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'remote_controller'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['remote_controller'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('resource/*')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Vincent Vandyck',
+    maintainer_email='vvandyck@robonautics.com',
+    description='PyQT5 based dev GUI for ROS2 frontseat functions',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'remote_controller = remote_controller.remote_controller:main',
+        ],
+    },
 )
-
-setup(**setup_args)
